0abdf1e98b9a45f8c574fb17d553da5b124fa1bb
lionless072 <lionless072@gmail.com>
2020-10-31 01:08:09 +0300

    preparing to localization for serializers

--- Hack2020/pet/serializer.py
+++ Hack2020/pet/serializer.py
@@ -1,11 +1,28 @@
 from rest_framework.serializers import ModelSerializer
 import rest_framework.serializers as ser
-from .models import Pet
+from .models import Pet, PetType
+
+
+class PetTypeSerializer(ModelSerializer):
+    class Meta:
+        model = PetType
+        fields = ['key']
+
+
+class CustomSerializer(ModelSerializer):
+    ...
 
 
 class PetSerializer(ModelSerializer):
+    # name = ser.CharField(label='кличка', )
+    def to_internal_value(self, data):
+        return
 
-    #name = ser.CharField(label='кличка', )
+    def to_representation(self, instance):
+        return {
+            'кличка': instance.name,
+            'тип шерсти': instance.pet_type.key,
+        }
 
     class Meta:
         model = Pet
