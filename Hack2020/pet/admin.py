26ad91811772990ac915d291c4a6adee6282bd05
lionless072 <lionless072@gmail.com>
2020-10-30 23:59:34 +0300

    model Pet v 0.2

--- Hack2020/pet/admin.py
+++ Hack2020/pet/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import Pet, PetType, SizeType, WoolType, TailType, EarType, ColorType, Vaccination, Treatment, Bread, \
+    AdministrationArea, PetOwner
+
 # Register your models here.
+
+admin.site.register(Pet)
+admin.site.register(PetOwner)
+admin.site.register(AdministrationArea)
+admin.site.register(Bread)
+admin.site.register(PetType)
+admin.site.register(SizeType)
+admin.site.register(WoolType)
+admin.site.register(TailType)
+admin.site.register(EarType)
+admin.site.register(ColorType)
+admin.site.register(Vaccination)
+admin.site.register(Treatment)
--- Hack2020/pet/models.py
+++ Hack2020/pet/models.py
@@ -1,45 +1,89 @@
 from django.db import models
 
+
 # Create your models here.
 
-PET_TYPES = (
-    ('dog', 'DOG'),
-    ('cat', 'CAT')
-)
+class AbstractTypeModel(models.Model):
+    key = models.CharField(max_length=120)
+
+    class Meta:
+        abstract = True
+
+
+class EarType(AbstractTypeModel):
+    class Meta:
+        abstract = False
+
+
+class TailType(AbstractTypeModel):
+    pass
+
+
+class WoolType(AbstractTypeModel):
+    pass
+
+
+class Bread(AbstractTypeModel):
+    pass
+
+
+class PetType(AbstractTypeModel):
+    class Meta:
+        abstract = False
+
+
+class ColorType(AbstractTypeModel):
+    pass
+
+
+class SizeType(AbstractTypeModel):
+    pass
+
+
+class AdministrationArea(AbstractTypeModel):
+    pass
+
+
+class PetOwner(models.Model):
+    name = models.CharField(max_length=128)
+
+
+# not in use
+class Vet(models.Model):
+    name = models.CharField(max_length=128)
 
 
 class Pet(models.Model):
-    accounting_card = models.CharField(max_length=20, unique=True, null=False)
-    pet_type = models.CharField(max_length=40, null=False, choices=PET_TYPES)
+    accounting_card = models.CharField(max_length=128, unique=True, null=False)
+    pet_type = models.ForeignKey(PetType, on_delete=models.SET_NULL, null=True)
     birthdate = models.DateField(null=False)
-    weight = models.ImageField(null=False)
+    weight = models.IntegerField(null=False)
     name = models.CharField(max_length=128)
-    gender
-    bread
-    color
-    wool_type
-    ears_type
-    tail_type
-    size_type
-    special_parametrs
-    avairy
+    gender = models.SmallIntegerField(choices=((0, 'Male'), (1, 'Female')))
+    bread = models.ForeignKey(Bread, on_delete=models.SET_NULL, null=True)
+    color = models.ForeignKey(ColorType, on_delete=models.SET_NULL, null=True)
+    wool_type = models.ForeignKey(WoolType, on_delete=models.SET_NULL, null=True)
+    ears_type = models.ForeignKey(EarType, on_delete=models.SET_NULL, null=True)
+    tail_type = models.ForeignKey(TailType, on_delete=models.SET_NULL, null=True)
+    size_type = models.ForeignKey(SizeType, on_delete=models.SET_NULL, null=True)
+    special_parameters = models.CharField(max_length=128)
+    avairy = models.IntegerField()
 
     # special info
-    id_label
-    socilzed
-    sterilized
-    sterilization_date
-    vet_nlp
-    socialized
+    id_label = models.CharField(max_length=128)
+    sterilized = models.BooleanField()
+    sterilization_date = models.DateField(null=True)
+    vet_nlp = models.CharField(max_length=128, null=True)
+    socialized = models.BooleanField()
 
     # catching
-    work_order
-    administration_area
-    catching_act
-    catching_adres
+    work_order = models.CharField(max_length=32)
+    administration_area = models.ForeignKey(AdministrationArea, on_delete=models.SET_NULL, null=True)
+    catching_act = models.CharField(max_length=32)
+    catching_address = models.CharField(max_length=256)
 
     # owners
-    ...
+    owner = models.ForeignKey(PetOwner, on_delete=models.SET_NULL, null=True)
 
     # health
     inspection_date = models.DateField(null=False)
@@ -47,15 +91,15 @@ class Pet(models.Model):
 
 
 class Treatment(models.Model):
-    pet = models.ForeignKey(Pet)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
     number = models.PositiveIntegerField()
     date = models.DateField(null=False)
     product_name = models.CharField(max_length=128)
-    dose = models.FloatField()
+    dose = models.CharField(max_length=20)
 
 
 class Vaccination(models.Model):
-    pet = models.ForeignKey(Pet)
+    pet = models.ForeignKey(Pet, on_delete=models.CASCADE)
     number = models.PositiveIntegerField()
     date = models.DateField(null=False)
     vac_type = models.CharField(max_length=128)
