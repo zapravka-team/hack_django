34b3ab6ab906fd4a4429f3bce9c605328af085ea
lionless072 <lionless072@gmail.com>
2020-11-01 11:52:43 +0300

    swagger loading

--- Hack2020/Hack2020/urls.py
+++ Hack2020/Hack2020/urls.py
@@ -3,11 +3,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from rest_framework_swagger.views import get_swagger_view
+auto_schema = get_swagger_view(title='swagger')
+
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('pets/', include('pet.urls')),
                   path('report/', include('report.urls')),
                   path('manual/', include('manual.urls')),
+                  path('swagger/', auto_schema),
                   path('api-auth/', include('rest_framework.urls')),
                   path('auth/', include('rest_auth.urls')),
                   path('auth/registration/', include('rest_auth.registration.urls'))
