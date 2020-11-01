007f6e216d605c4742d55285064c077b7e0c2755
lionless072 <lionless072@gmail.com>
2020-11-01 11:59:32 +0300

    last chance

--- Hack2020/Hack2020/urls.py
+++ Hack2020/Hack2020/urls.py
@@ -2,17 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from rest_framework.schemas.openapi import AutoSchema
 from rest_framework_swagger.views import get_swagger_view
-auto_schema = get_swagger_view(title='swagger')
+
+auto_schema = AutoSchema
 
 urlpatterns = [
                   path('admin/', admin.site.urls),
                   path('pets/', include('pet.urls')),
                   path('report/', include('report.urls')),
                   path('manual/', include('manual.urls')),
-                  path('swagger/', auto_schema),
                   path('api-auth/', include('rest_framework.urls')),
+                  path('swagger/', auto_schema),
                   path('auth/', include('rest_auth.urls')),
                   path('auth/registration/', include('rest_auth.registration.urls'))
               ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
